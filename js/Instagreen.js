901f1fde7cd8d0ba54ad5489b5a09741ebf27d3f
Neil <neilmonastiri@gmail.com>
2022-02-05 14:09:25 +0100

    first commit

---
+++ js/Instagreen.js
@@ -0,0 +1,15 @@
+function login(){
+    location.replace("../html/login.html");
+}
+
+function signUp(){
+    location.replace("../html/SignUp.html");
+}
+
+function acceuil(){
+    location.replace("../html/acceuil.html");
+}
+
+function uploadfile(){
+    location.replace("../html/uploadFile.html");
+}
